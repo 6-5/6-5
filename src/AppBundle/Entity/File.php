98d5c9b253e1022218d0893004cf9c8070a6d392
Dany Maillard <danymaillard93b@gmail.com>
2017-02-28 11:02:45 +0100

    Add File Entity

---
+++ src/AppBundle/Entity/File.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * File
+ *
+ * @ORM\Table(name="file")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FileRepository")
+ */
+class File
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
