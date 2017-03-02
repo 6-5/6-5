68a760622592ce00466a0d98ceeb12c1c35d5034
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2017-03-02 17:38:45 +0100

    Add translator service to UserManager

--- src/AppBundle/Manager/UserManager.php
+++ src/AppBundle/Manager/UserManager.php
@@ -3,8 +3,7 @@
 namespace AppBundle\Manager;
 
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\Translation\Translator;
-use Symfony\Component\Translation\MessageSelector;
+use Symfony\Component\Translation\TranslatorInterface;
 use AppBundle\Entity\User;
 
 class UserManager
@@ -12,7 +11,7 @@ class UserManager
     private $translator;
     private $requestStack;
 
-    public function __construct(Translator $translator, RequestStack $requestStack)
+    public function __construct(TranslatorInterface $translator, RequestStack $requestStack)
     {
         $this->translator = $translator;
         $this->requestStack = $requestStack;
@@ -20,6 +19,10 @@ class UserManager
 
     public function getRanks($abbreviated = false, $locale = null)
     {
+        //$userReflection = new \ReflectionClass(User::class);
+        //$constants = $userReflection->getConstants();
+        //return $constants;
+
         return;
     }
 
