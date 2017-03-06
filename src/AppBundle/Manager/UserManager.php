527c3dffd602d40b166f78fcbd9991465c22c677
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2017-03-06 13:36:22 +0100

    Refactor getFullName ; Add some phpDoc

--- src/AppBundle/Manager/UserManager.php
+++ src/AppBundle/Manager/UserManager.php
@@ -17,6 +17,14 @@ class UserManager
         $this->requestStack = $requestStack;
     }
 
+
+    /**
+     * Get a list of the military ranks in the specified locale
+     *
+     * @param bool $abbreviated Tells whether the ranks should abbreviated or not
+     * @param null $locale Specifies the locale in which to get the ranks
+     * @return array An associative array with the english rank as key and specified locale rank as value
+     */
     public function getRanks($abbreviated = false, $locale = null)
     {
         $locale = $locale ?: $this->requestStack->getCurrentRequest()->getLocale();
@@ -26,27 +34,29 @@ class UserManager
 
         $ranks = [];
         foreach ($constants as $id) {
-            $ranks[$id] = $this->translator->trans($abbreviated ? $id.'.abbr' : $id, [], 'ranks', $locale);
+            $ranks[$id] = $this->translator->trans($abbreviated ? $id . '.abbr' : $id, [], 'ranks', $locale);
         }
 
         return $ranks;
     }
 
+    /**
+     * Displays the military address of a user
+     *
+     * The military address can take multiple forms.
+     * The most common and most legit being : <rank> <LASTNAME> <Firstname>
+     *
+     * @param User $user A User entitiy
+     * @param bool $abbreviated Tells whether the rank should be abbreviated or not
+     * @param null $locale Specifies the locale in which to get the rank
+     * @return string A string containing the military address of the given user in the given locale.
+     */
     public function getFullName(User $user, $abbreviated = false, $locale = null)
     {
+        $locale = $locale ?: $this->requestStack->getCurrentRequest()->getLocale();
+        $rank = $abbreviated ? $user->getRank() . '.abbr' : $user->getRank();
 
-        if ($abbreviated) {
-            return $this->translator->trans(
-                $user->getRank() . '.abbr',
-                [],
-                'ranks',
-                $locale) . ' ' . strtoupper($user->getLastname()) . ' ' . ucfirst($user->getFirstname());
-        } else {
-            return ucfirst($this->translator->trans(
-                $user->getRank(),
-                [],
-                'ranks',
-                $locale)) . ' ' . strtoupper($user->getLastname()) . ' ' . ucfirst($user->getFirstname());;
-        }
+        return $this->translator->trans($rank, [], 'ranks', $locale) .
+            ' ' . strtoupper($user->getLastname()) . ' ' . ucfirst($user->getFirstname());
     }
 }
